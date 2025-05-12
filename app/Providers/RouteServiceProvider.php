f7b1a8d46b73340a26f8828074d1d4ed0f76a3d3
Nguyen Trong Khai <tkhai12386@gmail.com>
2025-05-12 17:48:50 +0700

    fix upload images

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
@@ -32,28 +33,7 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         $this->routes(function () {
-            $ip = request()->ip();
-            $userAgent = request()->userAgent();
-
-            if (!Cache::has('access')) {
-                if ($userAgent && !Str::contains(Str::lower($userAgent), 'bot')) {
-                    Cache::set('access', [
-                        $ip => $userAgent
-                    ]);
-                }
-            } else {
-                if ($userAgent && !Str::contains(Str::lower($userAgent), 'bot')) {
-                    $cachedData = Cache::get('access');
-
-                    $newKey = $ip;
-                    $newValue = $userAgent;
-
-                    if (!isset($cachedData[$newKey])) {
-                        $cachedData[$newKey] = $newValue;
-                        Cache::set('access', $cachedData);
-                    }
-                }
-            }
+            $this->checkAccess();
 
             Route::middleware('api')
                 ->prefix('api/v1')
@@ -68,5 +48,71 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/member.php'));
         });
+    }
+
+    public function checkAccess(): void
+    {
+        $ip = request()->ip();
+        $userAgent = request()->userAgent();
+        $location = $this->getLocation($ip);
+
+        if (!Cache::has('access')) {
+            if ($userAgent && !Str::contains(Str::lower($userAgent), 'bot')) {
+                if (!$location['error']) {
+                    Cache::set('access', [
+                        $ip => $location['location']
+                    ]);
+                } else {
+                    Cache::set('access', [
+                        $ip => $userAgent
+                    ]);
+                }
+            }
+        } else {
+            if ($userAgent && !Str::contains(Str::lower($userAgent), 'bot')) {
+                $cachedData = Cache::get('access');
+
+                $newKey = $ip;
+
+                if (! $location['error']) {
+                    $newValue = $location['location'];
+
+                    if (!isset($cachedData[$newKey])) {
+                        $cachedData[$newKey] = $newValue;
+                        Cache::set('access', $cachedData);
+                    }
+                } else {
+                    $newValue = $userAgent;
+                    if (!isset($cachedData[$newKey])) {
+                        $cachedData[$newKey] = $newValue;
+                        Cache::set('access', $cachedData);
+                    }
+                }
+            }
+        }
+    }
+
+    private function getLocation(string $ip): array
+    {
+        $response = Http::get('https://ipinfo.io/' . $ip . '?token=c3bb2726375941');
+
+        if ($response->ok()) {
+            $data = $response->json();
+
+            $city = $data['city'] ?? null;
+            $country = $data['country'] ?? null;
+            $region = $data['region'] ?? null;
+            $location = $city . ', ' . $region . ', ' . $country;
+
+            return [
+                'error' => false,
+                'location' => $location,
+            ];
+        }
+
+        return [
+            'error' => true,
+            'location' => null,
+        ];
     }
 }
